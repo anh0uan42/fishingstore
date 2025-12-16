3c09f85ed1527845cdd7ad1acc813927f7830867
Q Nguyen <nguyenanhquancttpcl@gmail.com>
2025-12-15 18:19:20 -0600

    jwt and access control test

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,7 +17,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // Your actual frontend URL
+  res.header('Access-Control-Allow-Origin', '*'); // Your actual frontend URL
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Authorization');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
